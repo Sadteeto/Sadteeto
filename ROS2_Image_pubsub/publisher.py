79c4b64976b03073eb7220e168380555d50d3889
Sadteeto <87579232+Sadteeto@users.noreply.github.com>
2021-10-10 18:35:10 +1000

    Update publisher.py

--- ROS2_Image_pubsub/publisher.py
+++ ROS2_Image_pubsub/publisher.py
@@ -12,7 +12,7 @@ class MinimalPublisher(Node):
     def __init__(self):
         super().__init__('minimal_publisher')
         self.publisher_ = self.create_publisher(Image, 'topic', 10)
-        timer_period = 0.01677777  # seconds
+        timer_period = 0.016666  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         
     
